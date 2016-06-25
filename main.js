a42392f94b3520fade77c5d33ba58a61a8b739b5
Synergymax <kevinsynergytw@gmail.com>
2016-06-25 17:55:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var bgImg = document.createElement("img");
+var jasonImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+jasonImg.src = "images/jason.gif";
+
+var jason = {
+  x:0,
+  y:0
+};
+
 function draw(){
 ctx.drawImage(bgImg,0,0);
+ctx.drawImage(jasonImg,jason.x,jason.y);
 }
 setTimeout(draw, 1000);
-
